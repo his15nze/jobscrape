e56471dfe68913f04df0c811c7fbec719cfc306b
Nils <47695458+his15nze@users.noreply.github.com>
2020-06-11 17:44:06 +0200

    la till en kommentar

--- firefox_jobscrape.py
+++ firefox_jobscrape.py
@@ -46,6 +46,8 @@ def reach_specific(driver, glassdoor_data):
     driver.get(glassdoor_data)
 
 # kommentar
+# kommentar
+
 def get_job_on_page(driver, job_vector): 
     '''
     for i in range(1,number_pages):
